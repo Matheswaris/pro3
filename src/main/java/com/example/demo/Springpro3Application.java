a665781571ce65bcff82362ec736febeda8fbade
Matheswaris <matheswari216@gmail.com>
2023-02-22 11:37:11 +0530

    First commit

---
+++ src/main/java/com/example/demo/Springpro3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springpro3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springpro3Application.class, args);
+	}
+
+}
